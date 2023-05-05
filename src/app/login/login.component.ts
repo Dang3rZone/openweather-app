57c7de61a298a5219f548284dd39d965f0bf052c
dang3rzone <andresrosalespinzon@gmail.com>
2023-05-05 12:20:43 +0200

    feat: started login logic with tempalte

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  public user!: string;
+  public password!: string;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  public onSubmit() {
+    if (this.authService.login(this.user, this.password)) {
+      this.router.navigate(['/main']);
+    } else {
+      // Mostrar mensaje de error
+    }
+  }
 }
