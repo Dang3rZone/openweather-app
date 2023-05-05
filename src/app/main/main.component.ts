5f43bf9889c4ec097de2e79a9b9119c857ab0f5e
dang3rzone <andresrosalespinzon@gmail.com>
2023-05-05 13:02:14 +0200

    feat:boilerplater main logic with template

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent {
+  public cities: string[] = [];
+  public selectedCity: string | null = null;
+  public weather: any = null;
 
+  constructor(private weatherService: WeatherService) {}
+
+  public onCitySelected(city: string) {
+    this.selectedCity = city;
+    this.weatherService.getWeather(city).subscribe((data) => {
+      this.weather = data;
+    });
+  }
+
+  public removeCity(city: string) {
+    this.cities = this.cities.filter((c) => c !== city);
+    if (this.selectedCity === city) {
+      this.selectedCity = null;
+      this.weather = null;
+    }
+  }
 }
