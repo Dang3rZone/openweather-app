c2703c16e7cda1d85eff82df2573967cb9b9e187
dang3rzone <andresrosalespinzon@gmail.com>
2023-05-05 13:01:48 +0200

    feat: boilerplate logic for weather service

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  private apiKey = '5b4a5fb7fff1a8f5a3c0cd68dc4e9a5b';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getWeather(city: string) {
+    const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`;
+    return this.http.get(url);
+  }
 }
