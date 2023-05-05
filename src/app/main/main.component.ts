7295c9a936f9b3457f87172be476cedc8ce700a1
dang3rzone <andresrosalespinzon@gmail.com>
2023-05-05 17:04:34 +0200

    feat:  fix scss tupo and delete bolier plate

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,27 +1,30 @@
 import { Component } from '@angular/core';
 import { WeatherService } from '../weather.service';
+import cities from '../cities/cities.json';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css'],
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
-  public cities: string[] = [];
+  public cities: string[] = cities;
   public selectedCity: string | null = null;
   public weather: any = null;
+  public selectedCities: string[] = [];
 
   constructor(private weatherService: WeatherService) {}
 
   public onCitySelected(city: string) {
     this.selectedCity = city;
+    this.selectedCities.push(city);
     this.weatherService.getWeather(city).subscribe((data) => {
       this.weather = data;
     });
   }
 
   public removeCity(city: string) {
-    this.cities = this.cities.filter((c) => c !== city);
+    this.selectedCities = this.selectedCities.filter((c) => c !== city);
     if (this.selectedCity === city) {
       this.selectedCity = null;
       this.weather = null;
